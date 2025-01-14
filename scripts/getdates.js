b8408dadd976a7c1a2b8042e8e2cedf98382cd42
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-01-14 13:57:53 -0700

    Finished week 1's requirements

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#currentyear")
+const modified = document.querySelector("#lastModified")
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+modified.innerHTML = document.lastModified;
